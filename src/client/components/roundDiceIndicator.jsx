b686d56e24cc089619202f70af85147986a3976e
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-11-11 14:12:38 +1300

    Added positioning of side bar child components

--- src/client/components/roundDiceIndicator.jsx
+++ src/client/components/roundDiceIndicator.jsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
-import "./sideBarChildComponents.css";
 
 // Child component of SideBar
 class RoundDiceIndicator extends Component {
   state = {};
   render() {
-    return <h1 className="subComponent">RoundDiceIndicator</h1>;
+    return <h1>RoundDiceIndicator</h1>;
   }
 }
 
--- src/client/components/sideBar.jsx
+++ src/client/components/sideBar.jsx
@@ -1,18 +1,37 @@
 import React, { Component } from "react";
 import RoundDiceIndicator from "./roundDiceIndicator";
 import NodeInfo from "./nodeInfo";
-import OtherPlayerInfo from "./otherPlayerInfo";
+import OtherPlayerInfo from "./otherPlayerInfo"; //TODO: Add this as a child component of PlayerInfo
 import ChatWindow from "./chatWindow";
 import BuyMenu from "./buyMenu";
+import "./sideBarChildComponents.css";
 
 class SideBar extends Component {
   state = {};
   render() {
     const style = {
       backgroundColor: "salmon",
+      display: "flex",
+      flexDirection: "column",
       flex: "1"
     };
-    return <div style={style}></div>;
+    return (
+      <div style={style}>
+        <div className="subComponentSmaller">
+          {/*Components are enclosed in divs so that the positioning is loosely coupled with the component */}
+          <RoundDiceIndicator />
+        </div>
+        <div className="subComponentRegular">
+          <ChatWindow />
+        </div>
+        <div className="subComponentRegular">
+          <BuyMenu />
+        </div>
+        <div className="subComponentRegular">
+          <NodeInfo />
+        </div>
+      </div>
+    );
   }
 }
 
