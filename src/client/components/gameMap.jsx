eb6b04ca4174b0db36fa9044bf6f89d28149cd5f
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-11-12 17:00:05 +1300

    Converted GameMap component to be a canvas for easier drawing of elements

--- src/client/components/gameMap.jsx
+++ src/client/components/gameMap.jsx
@@ -1,15 +1,38 @@
 import React, { Component } from "react";
 class GameMap extends Component {
   state = {};
+
+  componentDidMount() {
+    const canvas = this.refs.gameMapCanvas;
+    const canvasContext = canvas.getContext("2d");
+    //const gameMapCanvas = document.getElementById("gameMapCanvas");
+    //console.log(gameMapCanvas);
+    this.drawNode(canvasContext);
+  }
+
   handleGameMapClicked = () => {
     console.log("Game map clicked");
   };
+
+  // TODO: Temporary
+  drawNode = canvasContext => {
+    canvasContext.beginPath();
+    canvasContext.arc(100, 100, 5, 0, Math.PI * 2);
+    canvasContext.fillStyle = "blue";
+    canvasContext.fill();
+  };
+
   render() {
+    //const parentComponent = gameMap.parentNode();
+    //const computedStyles = window.getComputedStyle(gameMapCanvas);
+    //console.log(computedStyles);
     const style = {
       backgroundColor: "lightYellow",
       flex: "3"
     };
-    return <div style={style} onClick={this.handleGameMapClicked}></div>;
+    return (
+      <canvas ref="gameMapCanvas" id="gameMapCanvas" style={style}></canvas>
+    );
   }
 }
 
