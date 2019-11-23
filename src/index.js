e78ec92d6bab6e38d73c0330a74b1830300ed61c
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-11-23 14:33:53 +1300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Solis from './client/components/solis.jsx'
 import * as serviceWorker from './serviceWorker';
+import openSocket from "socket.io-client";
+
+// Creating the socket to connect the client to the server
+const socket = openSocket(`http://localhost:8000`);
+socket.on('welcome', () => {
+    alert("Welcome To Solis?");
+});
+export default socket;
 
 ReactDOM.render(<Solis />, document.getElementById('root'));
 serviceWorker.unregister();
