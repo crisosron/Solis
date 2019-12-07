dad913fd6dc324750ca9d26e1b4ce63d0d5872d4
Ron Crisostomo <rcrisostomo97@gmail.com>
2019-12-07 16:17:32 +1300

    Began re-working lobby

--- src/client/components/menu-components/lobby.jsx
+++ src/client/components/menu-components/lobby.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 import UserName from "./userName";
 import ColorOption from "./colorOption";
 import socket from "../../../index.js";
+import "./menuComponents.css"
 
 export default class Lobby extends Component {
   constructor(props) {
     super(props);
     let gameID = null;
+    console.log(this.props.match);
     this.state = {
       colorOptions: [],
       connectedPlayersUserNames: []
@@ -17,45 +19,27 @@ export default class Lobby extends Component {
   componentDidMount() {}
 
   render() {
+    console.log("About to render lobby");
     return (
-      <div className="centerStyle optionsDiv" id="lobby">
-        <h1>Lobby</h1>
-        <h2>GameID goes here</h2>
+      <div className="centerStyle">
+        <h1 className="title">Lobby</h1>
+        <h3>Game Code</h3>
+        <h2>{this.props.match.params.id}</h2>
+
         <div className="centerStyle optionsDiv">
 
-          {/*Color selection rendering*/}
           <div id="colorSelectorDiv">
-            <h3>Color Selection</h3>
-            <div id="colorSelectorDivOptions">
-              {this.state.colorSelectionActive && // Only render color options if settings have been confirmed
-                this.state.colorOptions.map(colorOption => {
-                  return (
-                    <ColorOption
-                      colorValue={colorOption}
-                      key={colorOption}
-                      id={`ColorOption ${colorOption}`}
-                    />
-                  );
-                })}
-            </div>
+            <h3>Select A Color</h3>
           </div>
-            
-          {/*Username rendering*/}
+
           <div id="connectedPlayersDiv">
             <h3>Connected Players</h3>
-            {this.state.connectedPlayersUserNames.length !== 0 && // cond && statement makes it so that the map array function only executes if there are connected users at all!
-              this.state.connectedPlayersUserNames.map(playerUserName => {
-                return (
-                  <UserName
-                    playerName={playerUserName}
-                    playerColor="#ffffff"
-                    key={playerUserName + " #ffffff"}
-                    thisPlayer={this}
-                  />
-                );
-              })}
           </div>
+
         </div>
+
+        <br />
+        <br />
         <Link>
           <button className="returnButton">Return</button>
         </Link>
