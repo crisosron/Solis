51384e55a49180fdd20e6a527dd31f789b4b2a2f
Ron Crisostomo <rcrisostomo97@gmail.com>
2020-01-07 19:01:34 +1300

    Added table to display 'this' player's resources

--- src/client/components/game-components/game-ui-components/playerInfo.jsx
+++ src/client/components/game-components/game-ui-components/playerInfo.jsx
@@ -1,9 +1,52 @@
 import React, { Component } from "react";
+ /*Note: Structure of thisPlayerInfo prop: (Can be found at getAllInfo method @ player.js)
+  thisPlayerInfo{
+    allInfo{
+      color: val
+      userName: val
+      resources: {
+        minerals: val
+        manpower: val
+        darkMatter: val
+        alloys: val
+        fuelCells: val
+      }
+    }
+  }
+  */
 class PlayerInfo extends Component {
   render() {
     return (
       <div>
         <h1>PlayerInfo</h1>
+        <h3>Resources</h3>
+        <table>
+          <tr>
+            <td>Minerals:</td>
+            <td>Value</td>
+          </tr>
+
+          <tr>
+            <td>Manpower</td>
+            <td>Value</td>
+          </tr>
+
+          <tr>
+            <td>Dark Matter</td>
+            <td>Value</td>
+          </tr>
+
+          <tr>
+            <td>Alloys</td>            
+            <td>Value</td>
+          </tr>
+
+          <tr>
+            <td>Fuel Cells</td>            
+            <td>Value</td>
+          </tr>
+
+        </table>
       </div>
     );
   }
